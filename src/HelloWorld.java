6bfc8e270932899c2439b726ac81d5163e5833c7
MrZebarth <kris.zebarth@ocsb.ca>
2017-09-18 12:53:30 -0400

    First Save

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * This is my hello world program.
+ * @author Zebarth
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This is a single line comment
+		/*
+		 * This is
+		 * a multiline
+		 * comment
+		 */
+		System.out.println("Hello World!");
+		System.out.print("This is my first program");
+		System.out.println("Isn't this fun??");
+		System.out.println("He said \"Do this now!\"");
+	}
+
+}
